f784de7bc3a0418f22ddb9ddf396e68b64ea0bab
Kentaro ku KUMAGAI <ku0522a@gmail.com>
2017-08-01 17:38:30 +0900

    ticks show up

--- App.js
+++ App.js
@@ -1,126 +1,138 @@
 import React from 'react';
-import * as Exponent from 'expo';
 
-import ReactFauxDOM from 'react-faux-dom'
+import Exponent from 'expo'
+const d3 = require( 'd3' )
 
-//import d3 from 'd3';
-import * as selection from 'd3-selection'
+// to avoid an error caused by query-selector which is used inside of ReactFauxDOM
+global.navigator.userAgent = ''
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+const ReactFauxDOM = require('./react-faux-dom/lib/ReactFauxDOM.js')
+const d3scale = require('d3-scale')
+//const d3line  = require( 'd3-line')
+const d3selection = require( 'd3-selection')
+const d3axis = require( 'd3-axis')
 
-function querySelector(selector, element) {
-  if ( !(element instanceof Object) ) {
-    return null
-  }
+import _ from 'lodash'
 
-  if (element.type.displayName === selector) {
-    return addDomMethods(element)
-  }
+let componentMapping = {
+  svg: Exponent.Svg,
+}
 
-  if ( element.props.children instanceof Array ) {
-    for ( let child of element.props.children ) {
-      const match = querySelector(selector, child)
-      if (match) {
-        return match
-      }
-    }
-    return null
-  } else {
-    return querySelector(selector, element.props.children)
+for (let k in Exponent.Svg) {
+  const m = k.match(/^[A-Z]/)
+  if (m) {
+    const lowerCased = m[0].toLowerCase() + k.substring(1)
+    componentMapping[lowerCased] = Exponent.Svg[k]
   }
 }
 
-function querySelectorAll(selector, element) {
-  let matched = []
+const margin = {top: 20, right: 0, bottom: 0, left: 0}
 
-  if (element instanceof Object) {
-    if (element.type instanceof Object && element.type.displayName === selector) {
-      matched.push( addDomMethods(element) )
-    } else {
-      if ( element.props.children instanceof Array ) {
-        for ( let child of element.props.children ) {
-          matched = matched.concat( querySelectorAll(selector, child) )
-        }
-        return matched
-      } else {
-        return matched.concat(querySelectorAll(selector, element.props.children))
-      }
-    }
-  }
+import { Dimensions, StatusBar, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native'
 
-  return matched
-}
+const {height, width} = Dimensions.get('window');
 
-let doc = null
-
-
-const ElementMap = {
-  Text,
-}
-global.document =  {
-  querySelector: (selector) => {
-    const e = querySelector(selector, doc)
-    console.log(e)
-    return e
-  },
-  querySelectorAll: (selector) => {
-    return querySelectorAll(selector, doc)
-  },
-  createElementNS: (_, name) => {
-    const attributes = {}
-    return React.createElement(
-      ElementMap[name],
-      attributes,
-    )
-  }
-}
-
-function addDomMethods(reactElement) {
-  return Object.assign({
-    ownerDocument: global.document,
-    querySelectorAll: function (selector) {
-      return querySelectorAll(selector, reactElement)
-    },
-    insertBefore: function (element, before) {
-      const dom = React.DOM
-      if (reactElement.props.children instanceof Array) {
-        this.props.children.push(element)
-      } else {
-        reactElement.props.children = [
-          this.props.children,
-          element
-        ]
-      }
-    },
-  }, reactElement)
-}
+const mydata = [
+        {date: new Date(2007, 3, 21), value: 0 },
+        {date: new Date(2007, 3, 22), value: Math.random() },
+        {date: new Date(2007, 3, 23), value: Math.random() },
+        {date: new Date(2007, 3, 24), value: Math.random() },
+        {date: new Date(2007, 3, 25), value: Math.random() },
+        {date: new Date(2007, 3, 26), value: Math.random() },
+        {date: new Date(2007, 3, 27), value: Math.random() },
+        {date: new Date(2007, 3, 30), value: Math.random() },
+        {date: new Date(2007, 4,  1), value: Math.random() },
+        {date: new Date(2007, 4,  2), value: Math.random() },
+        {date: new Date(2007, 4,  3), value: 1 },
+    ];
 
 export default class App extends React.Component {
-
-  onPress() {
-    this.forceUpdate()
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: null,
+      offset: 0,
+    }
   }
 
-  componentWillMount() {
-    doc = this.__d3tree = (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button
-        onPress={ _ => onPress() }
-        />
-      </View>
-    );
+  createFauxElement(name) {
+    console.log('creating fauxdom', name)
+    return new ReactFauxDOM.Element(name)
+  }
 
-    const theData = [1,2,3]
-      var p = selection.select("View")
-      .selectAll("Text")
-      .data(theData)
-      .enter().append("Text")
-        .text(function(d) { return "I'm number " + d + "!"; });
+
+  componentWillMount() {
+    const root = this.root = global.document = new ReactFauxDOM.Element('g')
+    root.appendChild( this.createFauxElement('g') )
+  }
+
+  updateD3Chart() {
+    var x = d3scale.scaleTime()
+      .domain([new Date(2007, 3, 21), new Date(2007, 4, 3)])
+      .range([0, width]);
+
+    var y = d3scale.scaleLinear()
+      .domain([0, 1])
+      .range([0, height - margin.top]);
+
+    //this.renderPath(x, y)
+
+    const svg = d3selection.select('g')
+
+    const g = svg.append("g")
+
+    var yAxis = d3axis.axisRight(y)
+                    .tickSize(width)
+    const customYAxis = function (g) {
+       g.call(yAxis);
+        g.select(".domain").remove();
+        g.selectAll(".tick line").attr("stroke", "#777").attr("stroke-dasharray", "2,2");
+        g.selectAll(".tick text").attr("x", 4).attr("dy", -4);
+    }
+
+    g.append("g")
+      .call(customYAxis);
+
+  }
+
+  renderPath(x, y) {
+    const line = d3.line()
+          .x(function(d) { return Math.floor( x(d.date) ); })
+          .y(function(d) { return Math.floor( y(d.value) ); });
+
+    var chartLine = line(mydata)
+    const path =  this.createFauxElement('path')
+    path.setAttribute('d', chartLine  )
+    path.setAttribute('stroke', "#080"   )
+    path.setAttribute('fill', "transparent"   )
+
+    const fillPath = chartLine.replace(/^M/, `M0,${height}L`) +  `L${width},${height}`
+    const fill =  this.createFauxElement('path')
+    fill.setAttribute('d', fillPath  )
+    fill.setAttribute('stroke', "transparent"   )
+    fill.setAttribute('fill', "#20c520"   )
+
+    //const tickY = document.getElementById('y-tick')
+    //const tickY = global.document.getElementById('y-tick')
+    //const e = global.document.getElementById('chart')
+    //e.insertBefore(path, tickY)
+    //e.insertBefore(fill, tickY)
   }
 
   render() {
-    return this.__d3tree
+    this.updateD3Chart()
+
+    const svgElements = this.root.toReactNative(0, componentMapping)
+
+    return (
+      <View style={ {flex: 1, top: margin.top} }>
+        <Exponent.Svg width={width} height={height} style={ {
+          backgroundColor: '#efe'
+        }}>
+          {svgElements}
+        </Exponent.Svg>
+      </View>
+    )
   }
 }
 
